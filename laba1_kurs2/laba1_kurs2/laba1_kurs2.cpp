624c40c34c7cd7ca1ba72bfac09a93886357a8fb
FateFlow <pochka3858@gmail.com>
2024-12-11 19:31:48 +0300

    third commit

--- laba1_kurs2/laba1_kurs2/laba1_kurs2.cpp
+++ laba1_kurs2/laba1_kurs2/laba1_kurs2.cpp
@@ -30,17 +30,17 @@ public:
 void calculate_z1(double& z1, double m) {
     try {
         if (m < 0) {
-            throw CustomException("Ошибка: m не может быть отрицательным.", m, 1); // Строка 27
+            throw CustomException("Ошибка: m не может быть отрицательным.", m, 1);
         }
         double numerator = sqrt(pow((3 * m + 2), 2) - (24 * m));
-        double denominator = (3 * sqrt(m)) - (2 * sqrt(m));
+        double denominator = (3 * sqrt(m)) - (2 / sqrt(m)); 
         if (denominator == 0) {
-            throw CustomException("Ошибка: деление на ноль.", denominator, 2); // Строка 31
+            throw CustomException("Ошибка: деление на ноль.", denominator, 2);
         }
         z1 = numerator / denominator;
     }
     catch (const CustomException& e) {
-        std::cerr << "Исключение: " << e.what() << " Значение: " << e.getValue() << " Код ошибки: " << e.getCode_of_error() << std::endl; // Строка 36
+        std::cerr << "Исключение: " << e.what() << " Значение: " << e.getValue() << " Код ошибки: " << e.getCode_of_error() << std::endl;
         z1 = NAN; // Устанавливаем z1 в NaN в случае ошибки
     }
 }
@@ -49,19 +49,19 @@ void calculate_z1(double& z1, double m) {
 void calculate_z2(double& z2, double m) {
     try {
         if (m < 0) {
-            throw CustomException("Ошибка: m не может быть отрицательным.", m, 1); // Строка 45
+            throw CustomException("Ошибка: m не может быть отрицательным.", m, 1);
         }
         z2 = -sqrt(m);
     }
     catch (const CustomException& e) {
-        std::cerr << "Исключение: " << e.what() << " Значение: " << e.getValue() << " Код ошибки: " << e.getCode_of_error() << std::endl; // Строка 49
+        std::cerr << "Исключение: " << e.what() << " Значение: " << e.getValue() << " Код ошибки: " << e.getCode_of_error() << std::endl;
         z2 = NAN; // Устанавливаем z2 в NaN в случае ошибки
     }
 }
 
 int main() {
     setlocale(0, "russian");
-    double m = 0;
+    double m = 4.0; // Положительное значение m
     double z1, z2;
     calculate_z1(z1, m);
     calculate_z2(z2, m);
